1f4607e7e1db9f5e53bf9930cd0d4902447f85d8
talpat <talonpatterson94@gmail.com>
2019-10-12 23:35:27 +0200

    ia working

--- extras/TalonStuff.ml
+++ extras/TalonStuff.ml
@@ -48,3 +48,111 @@ let checkBoard b =
 			else aux b (i+1)
 	in
 	aux b 0
+
+(* ai Stuff *)
+
+let get_1_2 (a,_) = a
+let get_2_2 (_,a) = a
+
+let willWin l tok =
+	let winCon = [
+		(0, 1, 2);
+		(3, 4, 5);
+		(6, 7, 8);
+		(0, 3, 6);
+		(1, 4, 7);
+		(2, 5, 8);
+		(0, 4, 8);
+		(2, 4, 6);
+	] in
+	let rec aux tok = function
+		| [] -> (-1)
+		| (a, b, c)::e ->
+			if (List.nth l a = tok && List.nth l b = tok && List.nth l c = E)
+			then c
+			else if	(List.nth l a = tok && List.nth l b = E && List.nth l c = tok)
+			then b
+			else if (List.nth l a = E && List.nth l b = tok && List.nth l c = tok)
+			then a
+			else aux tok e
+	in
+	aux tok winCon
+
+let preventWin l tok =
+	let winCon = [
+		(0, 1, 2);
+		(3, 4, 5);
+		(6, 7, 8);
+		(0, 3, 6);
+		(1, 4, 7);
+		(2, 5, 8);
+		(0, 4, 8);
+		(2, 4, 6);
+	] in
+	let enemy = match tok with
+		| X -> O
+		| _ -> X
+	in
+	let rec aux tok = function
+		| [] -> (-1)
+		| (a, b, c)::e ->
+			if (List.nth l a = enemy && List.nth l b = enemy && List.nth l c = E)
+			then c
+			else if	(List.nth l a = enemy && List.nth l b = E && List.nth l c = enemy)
+			then b
+			else if (List.nth l a = E && List.nth l b = enemy && List.nth l c = enemy)
+			then a
+			else aux tok e
+	in
+	aux tok winCon
+
+let setupWin l tok =
+	if (List.nth l 2 = tok && List.nth l 6 = tok && List.nth l 1 = E && List.nth l 3 = E) ||
+		(List.nth l 2 = tok && List.nth l 8 = tok && List.nth l 1 = E && List.nth l 4 = E) ||
+		(List.nth l 6 = tok && List.nth l 8 = tok && List.nth l 3 = E && List.nth l 4 = E)
+	then 0
+	else if (List.nth l 2 = tok && List.nth l 6 = tok && List.nth l 5 = E && List.nth l 7 = E) ||
+		(List.nth l 2 = tok && List.nth l 0 = tok && List.nth l 5 = E && List.nth l 4 = E) ||
+		(List.nth l 6 = tok && List.nth l 0 = tok && List.nth l 7 = E && List.nth l 4 = E)
+	then 8
+	else if (List.nth l 0 = tok && List.nth l 8 = tok && List.nth l 1 = E && List.nth l 5 = E) ||
+	(List.nth l 0 = tok && List.nth l 6 = tok && List.nth l 1 = E && List.nth l 4 = E) ||
+	(List.nth l 8 = tok && List.nth l 6 = tok && List.nth l 5 = E && List.nth l 4 = E)
+	then 2
+	else if (List.nth l 0 = tok && List.nth l 8 = tok && List.nth l 3 = E && List.nth l 7 = E) ||
+		(List.nth l 0 = tok && List.nth l 2 = tok && List.nth l 3 = E && List.nth l 4 = E) ||
+		(List.nth l 8 = tok && List.nth l 2 = tok && List.nth l 7 = E && List.nth l 4 = E)
+	then 6
+	else (-1)
+
+let anyPlace l tok =
+	let rec aux = function
+		| n -> if (List.nth l n = E)
+			then n
+			else aux (n+1)
+		| 9 -> 9
+	in
+	if List.nth l 0 = E
+	then 0
+	else if List.nth l 2 = E
+	then 2
+	else if List.nth l 6 = E
+	then 6
+	else if List.nth l 8 = E
+	then 8
+	else aux 1
+
+let findTarget l tok =
+	if (willWin l tok) >= 0
+	then (willWin l tok)
+	else if (preventWin l tok) >= 0
+	then (preventWin l tok)
+	else if (setupWin l tok) >= 0(* find position that lies in 2 winConditions each with on tok and one E *)
+	then (setupWin l tok)
+	else anyPlace l tok(* place in first available position *)
+
+
+let placeToken ia b =
+	let targetMap = findTarget (List.nth b 9) (get_1_2 ia) in
+	let targetPoint = findTarget (List.nth b targetMap) (get_1_2 ia) in
+	(targetMap+1, targetPoint+1)
--- src/main.ml
+++ src/main.ml
@@ -10,6 +10,32 @@ let place_map token pos map =
 let place_board board token (x, y) =
 	List.mapi (fun i a -> if i = (x - 1) then place_map token y a else a) board
 
+let rec iaMain board pl =
+	begin
+	match pl with
+	| O -> print_string "O's turn to play.\n"
+	| X -> print_string "X's turn to play.\n"
+	end;
+	let input =
+		if pl = X
+		then get_input () 
+		else placeToken (pl, 0) board
+	in
+	let new_board = place_board board pl input in
+	if (board = new_board) then
+		begin	
+			print_string "Invalid placement\n";
+			iaMain board pl
+		end
+	else
+		begin
+			let new_board = checkBoard new_board in
+			print_char '\n';
+			print_board new_board;
+			print_char '\n';
+			iaMain new_board (if pl = O then X else O)
+		end
+
 let rec main board pl =
 	begin
 	match pl with
@@ -31,10 +57,13 @@ let rec main board pl =
 			print_char '\n';
 			main new_board (if pl = O then X else O)
 		end
+
 let () =
 	let new_map = [E; E; E; E; E; E; E; E; E] in
 	let board = [new_map; new_map; new_map; new_map; new_map; new_map; new_map; new_map; new_map; new_map] in
 	print_board board;
 	print_char '\n';
-	main board O;
+	if Array.length Sys.argv > 1 && Sys.argv.(1) = "ia"
+	then iaMain board O
+	else main board O
 	()

d2c6f1bfe66780aaeb9e951e53795b8640b5854c
talpat <talonpatterson94@gmail.com>
2019-10-12 19:30:09 +0200

    full board check

--- extras/talon_stuff.ml
+++ extras/talon_stuff.ml
@@ -1,7 +1,9 @@
 type token = X | O | E
 type map = token list
-type mapTup
 type board = map list
+
+let replace l pos a =
+	List.mapi (fun i x -> if i = pos then a else x) l
 
 let checkMap m = 
 	let winCon = [
@@ -26,3 +28,52 @@ let checkMap m =
 			else aux m e
 	in
 	aux m winCon
+
+let checkBoard b =
+	let rec aux b = function
+		| 9 ->
+			print_string "";
+			b
+		| _ as i ->
+			if checkMap (List.nth b i) != E && List.nth (List.nth b 9) i == E
+			then begin (* print "x wins grid 4!" and modify gamestate*)
+				match checkMap (List.nth b i) with
+					| X ->
+						print_endline ("X wins grid " ^ (string_of_int i));
+						replace b 9 (replace (List.nth b 9) i X)
+					| O ->
+						print_endline ("O wins grid " ^ (string_of_int i));
+						replace b 9 (replace (List.nth b 9) i O)
+			end
+			else aux b (i+1)
+	in
+	aux b 0
+
+let () =
+	match checkBoard ([
+[X;X;X; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+]) with | _ -> ()
+		
+(* test list:
+[
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+[E;E;E; E;E;E; E;E;E];
+] 
+*)
